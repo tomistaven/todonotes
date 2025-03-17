181d3171ea6f6fac1e4509342aea36fd83014454
Tomi Stavén <tomistaven@gmail.com>
2025-03-17 23:34:30 +0200

    d

--- mytodonotes/pages/404.js
+++
@@ -1,9 +0,0 @@
-import { createElement } from "../../framework/core/dom.js";
-
-export function render404() {
-  return createElement("div", { className: "page-404" }, [
-    createElement("h1", {}, ["404 - Page Not Found"]),
-    createElement("p", {}, ["The page you are looking for does not exist."]),
-    createElement("button", { onClick: () => router.navigate("/") }, ["Go Home"]),
-  ]);
-}
